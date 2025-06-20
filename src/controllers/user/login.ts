f858818fa2eab4dd21da2bf9328fffc120af0c42
axel <belaxist@gmail.com>
2025-06-20 23:31:01 +0700

    cookie domain update

--- src/controllers/user/login.ts
+++ src/controllers/user/login.ts
@@ -36,6 +36,7 @@ export const userLogin = async (
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", 
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      domain: process.env.COOKIE_DOMAIN || undefined,
     });
     res.status(200).json({ message: "Login successful", token });
   } catch (err) {
